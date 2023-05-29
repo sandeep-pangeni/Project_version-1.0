b87756a966ca818f73ff219348dfcecb7a16c899
sandeep.pasal <sandeep.pasalnp@gmail.com>
2023-05-29 15:40:54 +0545

    extendThemes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom"
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme, theme } from '@chakra-ui/react'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const themes = extendTheme({ colors })
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ChakraProvider>
+  <ChakraProvider theme={themes}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
